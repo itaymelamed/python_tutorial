fee255f66fa9204668eea8b77c5ba3c80a4c429f
Itay Melamed <itaymelamed@minutes-MacBook-Pro.local>
2018-12-16 20:42:10 +0200

    sds

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,3 +4,8 @@ import requests
 def test_ping():
     r = requests.get('http://web:5000/ping')
     assert r.text == 'pong'
+
+
+def test_ping_go():
+    r = requests.get('http://go:80')
+    assert r.text == 'pong'
