dc747f678f28229943bb39e332ad346235e6d510
Itay Melamed <itaymelamed@minutes-MacBook-Pro.local>
2018-12-16 20:45:54 +0200

    added go tests

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong", r.URL.Path)
+		fmt.Fprintf("pong")
 	})
 
 	http.ListenAndServe(":80", nil)
