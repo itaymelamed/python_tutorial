948504203f54c33237f005a3f6922d454b34134f
itaymelamed <itaymelamed@ymail.com>
2019-08-16 20:06:03 +0300

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,8 +4,3 @@ import requests
 def test_ping():
     r = requests.get('http://web:5000/ping')
     assert r.text == 'pong'
-
-
-def test_ping_go():
-    r = requests.get('http://go:80')
-    assert 'podng' in r.text
